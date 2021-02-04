94dbd166077f144fdb7b43bafda924ca76e972f6
Richard Saavedra <richard@WIN-BK3NT50V51U.localdomain>
2021-02-04 15:38:31 -0500

    server.js solve

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/angular'));
+ngApp.use(express.static('./dist/torreapp'));
 ngApp.get('/*', function (request, response) {
-	    response.sendFile(path.join(__dirname, '/dist/angular/index.html'));
-	);
-ngApp.listen(process.env.PORT 11 8080);
+	    response.sendFile(path.join(__dirname, '/dist/torreapp/index.html'));
+	);
+ngApp.listen(process.env.PORT || 8080);
