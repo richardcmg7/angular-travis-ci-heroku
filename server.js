65eac78443b56af93b1e697e9c32928f149e41cb
Richard Saavedra <richardcmg7@gmail.com>
2021-02-04 15:22:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/angular'));
-ngApp.get('/*', function (request, response) {
-	    response.sendFile(path.join(__dirname, '/dist/angular/index.html')););
-ngApp.listen(process.env.PORT || 8080);
+ngApp.use(express.static('./dist/torreapp'));
+ngApp.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/torreapp/'})});
+  ngApp.listen(process.env.PORT || 8080);
