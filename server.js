43f00f42c3675f0cff5728dbf9c9953c6760af81
Richard Saavedra <richard@WIN-BK3NT50V51U.localdomain>
2021-02-04 15:53:15 -0500

    adjust

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/torreapp'));
-ngApp.get('/*', function (request, response) {
-	    response.sendFile(path.join(__dirname, '/dist/torreapp/index.html'));
-	);
+ngApp.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/torreapp/'})});
 ngApp.listen(process.env.PORT || 8080);
+
