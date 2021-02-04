0e925bfc97826c3cf999512d9c2f308fcb089062
Richard Saavedra <richardcmg7@gmail.com>
2021-02-04 15:16:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/angular'));
 ngApp.get('/*', function (request, response) {
-	    response.sendFile(path.join(__dirname, '/dist/angular/index.html'));
-	);
-ngApp.listen(process.env.PORT 11 8080);
+	    response.sendFile(path.join(__dirname, '/dist/angular/index.html')););
+ngApp.listen(process.env.PORT || 8080);
